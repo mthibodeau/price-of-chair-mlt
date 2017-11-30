4782f57262a88f50f1535eafd637a8e3da098097
mthibodeau <megthibodeau@gmail.com>
2017-11-29 21:38:52 -0800

    Fixed typo

--- src/models/alerts/constants.py
+++ src/models/alerts/constants.py
@@ -8,5 +8,5 @@ COLLECTION = "alerts"
 
 
 URL = os.environ.get('MAILGUN_URL')
-API_KEY = os.environ.get('MAILFUN_API_KEY')
+API_KEY = os.environ.get('MAILGUN_API_KEY')
 FROM = os.environ.get('MAILGUN_FROM')
